b3be2178bc996d5bc9c631d9b3c6cf7541161e84
ZitronCrazy <zitro5@protonmail.com>
2021-02-25 21:38:00 +0100

    KeywordsAndExpressions

---
+++ src/KeywordsAndExpression/Task.java
@@ -0,0 +1,15 @@
+package KeywordsAndExpression;
+
+public class Task {
+
+    public static void task(String[] args) {
+        // In the following code that I will type below, write down what parts of the Code are expressions.
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+
+            // expressions: score = 100, score > 99, score = 0.
+        }
+    }
+}
